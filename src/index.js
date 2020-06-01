779f14171bd0482adf476695fe0c8a530fcc8bbd
Derek <derekluu90@gmail.com>
2020-06-01 17:32:59 -0400

    added bcrypt for password hashing

--- src/index.js
+++ src/index.js
@@ -18,3 +18,21 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on', port)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+  const password = "red12345!"
+  // hashing converts plain text passswords into unreadable strings, cannot be reverse to plain text password
+  // second argument = number of rounds, the number of the times the hashing algorithm is executed
+  const hashedPassword = await bcrypt.hash(password, 8)
+
+  console.log(password)
+  console.log(hashedPassword)
+
+  // compare hashes plain text password and compares with hashedpassword to authenticate users
+  const isMatch = await bcrypt.compare('red12345!', hashedPassword)
+  console.log(isMatch)
+}
+
+myFunction()
