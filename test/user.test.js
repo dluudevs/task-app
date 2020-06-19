f304715affbb9859462ac693fa9b749dbc49c909
Derek <derekluu90@gmail.com>
2020-06-18 21:49:21 -0400

    added tests for authentication routes

--- test/user.test.js
+++ test/user.test.js
@@ -1,13 +1,23 @@
 // supertest is a library created by express that allows us to easily run our test server / tests
 // otherwise we would have to run the server and tests separately
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+// use mongoose method to create new instance of objectid
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+  _id: userOneId,
   name: 'Mike',
   email: 'mike@example.com',
-  password: '56what!!'
+  password: '56what!!',
+  tokens: [
+    {
+      token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }
+  ]
 }
 
 // runs before each test case
@@ -15,6 +25,7 @@ beforeEach(async () => {
   // no argument passed to deleteMany delete everything inside of the User collection
   // with await, jest will only move on to test cases when promise is resolved (users are all deleted)
   await User.deleteMany()
+  // save a new user 
   await new User(userOne).save()
 })
 
@@ -41,4 +52,35 @@ test('should not login nonexistent user', async () => {
     email: 'randomEmail@randomDomain.com',
     password: 'randomPW123!'
   }).expect(400)
+})
+
+test('should get profile for user', async () => {
+  await request(app)
+    .get('/users/me')
+    // sets header
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+  await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
+
+test('Should delete profile for authenticated user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should not delete profile for unauthenticated user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401)
 })
